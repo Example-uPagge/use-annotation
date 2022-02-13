11c911ef058f1aebfd1d9304de6e9a5d5994297e
Struchkov Mark <git@upagge.ru>
2022-02-13 17:22:23 +0300

    InitCommit

---
+++ src/main/java/dev/struchkov/example/useannotation/TestEntity.java
@@ -0,0 +1,12 @@
+package dev.struchkov.example.useannotation;
+
+import dev.struchkov.example.annotation.FieldNames;
+
+@FieldNames
+public class TestEntity {
+
+    private Long id;
+    private String title;
+    private String phoneNumber;
+
+}
